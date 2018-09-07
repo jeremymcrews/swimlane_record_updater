025be6ed1b423dd6ca44192dec56414f15e91b8f
Jeremy Crews <jeremyc@phoenixnap.com>
2018-09-07 13:05:43 -0700

    Change for version

--- swimlane_records_updater/SruRecords.py
+++ swimlane_records_updater/SruRecords.py
@@ -6,7 +6,19 @@ class Records:
         self.swimlaneUser = swimlaneUser
         self.swimlanePass = swimlanePass
         self.swimlaneHost = swimlaneHost
-        self.swimlane = swimlane = Swimlane(self.swimlaneHost, self.swimlaneUser, self.swimlanePass, verify_ssl=verifySsl)
+        self.swimlane = Swimlane(self.swimlaneHost, self.swimlaneUser, self.swimlanePass, verify_ssl=verifySsl)
+        self.apps = {}
+        self.records = {}
 
     def test(self):
         print self.swimlane
+
+    def getApp(self, appName, appId):
+        self.apps = self.swimlane.apps.get(id=appId)
+        return self.apps
+
+
+    def getRecord(self, appName, appId, recordId):
+        self.getApp(self, appName, appId)
+        self.records = self.apps[appName].records.get(id=recordId)
+        print self.apps
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from swimlane import Swimlane
 from swimlane_records_updater.SruRecords import Records
+import os
 
+os.environ['HTTPS_PROXY'] = 'http://10.111.64.98:3128'
+recordUpdater = Records('internalapiuser', '900576f8b34557d7193170a2835b28a9ac5d397c3bdaf6e389f4d59830e90826', 'https://phx0-psspsl01.pss.phoenixnap-internal.com')
+appData = recordUpdater.getApp('SEV2', 'aP0pSxO_qFQhNWteI')
+print recordUpdater.apps['SEV2']
 
-recordUpdater = Records('nternalapiuser', '900576f8b34557d7193170a2835b28a9ac5d397c3bdaf6e389f4d59830e90826', 'https://phx0-psspsl01.pss.phoenixnap-internal.com')
