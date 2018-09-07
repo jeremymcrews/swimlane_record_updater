48342fe6f86056dea19cf67de4a5cbd285e393b8
Jeremy Crews <jeremyc@phoenixnap.com>
2018-09-07 11:06:08 -0700

    Change for version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="swimlane_record_updater",
-    version="0.2",
+    name="swimlane_records_updater",
+    version="0.1",
     author="Jeremy m Crews",
     author_email="jeremy.m.crews@gmail.com",
     description="Common Record Updater for Swimlane apps",
--- swimlane_record_updater/__init__.py
+++
@@ -1,1 +0,0 @@
-name = "swimlane_record_updater"
