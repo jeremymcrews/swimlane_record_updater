78d3cc3b5f5ee5a1e77288b81eb07a5910dff228
Jeremy Crews <jeremyc@phoenixnap.com>
2018-09-14 12:41:28 -0700

    Version 2018.9.7: moving function from one class to another.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swimlane_records_updater",
-    version="2018.9.6",
+    version="2018.9.7",
     author="Jeremy m Crews",
     author_email="jeremy.m.crews@gmail.com",
     description="Common Record Updater for Swimlane apps",
